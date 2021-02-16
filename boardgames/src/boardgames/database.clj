93a45dbf3148eff99fe939593d62e04f0fce7f4a
anape <anape@DESKTOP-A9I01QD>
2021-02-16 16:24:21 +0100

    db connection

---
+++ boardgames/src/boardgames/database.clj
@@ -0,0 +1,18 @@
+(ns boardgames.database
+  (:require [clojure.java.jdbc :as sql]))
+
+(def connection
+  {:classname "com.mysql.jdbc.Driver"
+   :subprotocol "mysql"
+   :subname "//localhost:3306/boardgames?autoReconnect=true&useSSL=false"
+   :user "root"
+   :password ""})
+
+(defn get-all-boardgames []
+  (into [] (sql/query connection ["select * from boardgame"])))
+
+(defn display-all-boardgames []
+  (for [boardgame (get-all-boardgames)]
+     (println (:name boardgame) (:category boardgame) (:price boardgame) (:description boardgame))
+        )
+  )
